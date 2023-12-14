09b1aadd85d4275b80afe226285b73a7a0777522
Hiep.Nguyen <hlnguyen504@gmail.com>
2023-12-13 20:49:41 -0500

    ratings python file, query stars rating

---
+++ ratings.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from sqlite3 import dbapi2 as sq3
+
+connection = sq3.connect('yelp_database.db')
+
+cursor = connection.cursor()
+
+query = "SELECT * FROM businesses ORDER BY stars DESC LIMIT 10"
+
+df = pd.read_sql_query(query, connection)
+
+
+print(df[['business_id', 'name', 'stars']])
+
+
+#print(df.head()['categories'])
+#print(df.head()['DietaryRestrictions'])
+
+#cursor.execute("SELECT sql FROM sqlite_master WHERE type='table' AND name='businesses'")
+#print(cursor.fetchone()[0])
+
+connection.close()
